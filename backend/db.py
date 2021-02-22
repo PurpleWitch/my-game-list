1026745c8a62470c6672bec0454efdabe63680c5
hamdalla2 <hamdalla2@yahoo.com>
2021-02-22 11:32:32 +0200

    .

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,5 @@
 from flask_pymongo import pymongo
 from flask import Flask
 import urllib.parse
+
+client = pymongo.MongoClient("mongodb+srv://xoro:"+urllib.parse.quote("p@ssvv0rd")+"@cluster0.rphug.mongodb.net/Portfolio?retryWrites=true&w=majority")
