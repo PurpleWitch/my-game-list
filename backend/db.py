304ae1235f4932c22edfffa6660469cfbd2bf6a3
hamdalla2 <hamdalla2@yahoo.com>
2021-02-23 13:43:21 +0200

    .

--- backend/db.py
+++ backend/db.py
@@ -3,3 +3,4 @@ from flask import Flask
 import urllib.parse
 
 client = pymongo.MongoClient("mongodb+srv://xoro:"+urllib.parse.quote("p@ssvv0rd")+"@cluster0.rphug.mongodb.net/Portfolio?retryWrites=true&w=majority")
+db = pymongo.database.Database(client, 'database')
