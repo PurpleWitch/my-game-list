6d0fc069220c37fee13bdd85e9d70989788f65e7
hamdalla2 <hamdalla2@yahoo.com>
2021-03-04 23:10:58 +0200

    .

--- backend/db.py
+++ backend/db.py
@@ -4,3 +4,4 @@ import urllib.parse
 
 client = pymongo.MongoClient("mongodb+srv://xoro:"+urllib.parse.quote("p@ssvv0rd")+"@cluster0.rphug.mongodb.net/Portfolio?retryWrites=true&w=majority")
 db = pymongo.database.Database(client, 'database')
+col = pymongo.collection.Collection(db, 'collection')
