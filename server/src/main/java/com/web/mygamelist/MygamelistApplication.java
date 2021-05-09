708944c4bf6f285e8cea323ebab19b4f59ba0013
hamdalla2 <hamdalla2@yahoo.com>
2021-05-09 13:20:03 +0300

    spring init

---
+++ server/src/main/java/com/web/mygamelist/MygamelistApplication.java
@@ -0,0 +1,13 @@
+package com.web.mygamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygamelistApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/web/mygamelist/MygamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.mygamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
