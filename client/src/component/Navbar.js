30fe46c0193c3e5fb5eff07a8e03aa0ea66b62da
hamdalla2 <hamdalla2@yahoo.com>
2021-05-14 14:37:44 +0300

    navbar init

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
+    componentDidMount(){
+        this.setState({user:localStorage.getItem("user")})
+    }
     render() {
         return (
+            <div className="col">
             <div className="row">
-                <div className="menuitem">Sort</div>
-                <div className="menuitem">List</div>
-                <div className="menuitem">Sign</div>
+                <div className="menuitem">My Game List</div>
+                {this.state.user?<div>user</div>:<div>Sign</div>}
+            </div>
+            <div className="row">
+                <div className="menuitem">Online</div>
+                <div className="menuitem">Offline</div>
+                <div className="menuitem">Community</div>
+                <div className="menuitem">Download</div>
+                <div className="menuitem">Play</div>
+                <div className="menuitem">Search</div>
+            </div>
             </div>
         )
     }
