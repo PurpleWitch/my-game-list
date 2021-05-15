66731e8c65aa5ff29334efe29bf31aad2314f8d9
hamdalla2 <hamdalla2@yahoo.com>
2021-05-15 23:17:39 +0300

    sign

--- client/src/component/Sign.js
+++ client/src/component/Sign.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Sign extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="col">
+                <div className="row"><p>User</p><input type="text"></input></div>
+                <div className="row"><p>Pass</p><input type="text"></input></div>
+                <div className="row"><button>Sign in</button><button>Sign up</button></div>
             </div>
         )
     }
