d5ff794a02318854ba8078c914ad16df498453bf
hamdalla2 <hamdalla2@yahoo.com>
2021-05-16 12:34:40 +0300

    .

--- client/src/component/Sign.js
+++ client/src/component/Sign.js
@@ -6,7 +6,7 @@ export default class Sign extends Component {
             <div className="col">
                 <div className="row"><p>User</p><input type="text"></input></div>
                 <div className="row"><p>Pass</p><input type="text"></input></div>
-                <div className="row"><button>Sign in</button><button>Sign up</button></div>
+                <div className="row"><button onClick={this.sign}>Sign in</button><button onClick={this.sign}>Sign up</button></div>
             </div>
         )
     }
